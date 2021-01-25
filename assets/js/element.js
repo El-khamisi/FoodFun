81d1051685b52bc95396500e3069145345a7f9c1
MennaMagdy2019 <mena01096757508@gmail.com>
2021-01-25 06:35:26 +0200

    Finished

--- assets/js/element.js
+++ assets/js/element.js
@@ -64,3 +64,19 @@ function closeAllSelect(elmnt) {
     }
 }
 document.addEventListener("click", closeAllSelect);
+
+
+/*********btnTop**********/
+var btnTop = document.getElementById("topBtn");
+var fixedmenu = this.document.getElementById("header");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 700){
+        btnTop.style.display = "Block"
+    }
+    else{
+        btnTop.style.display = "none"
+    }
+}
+btnTop.addEventListener("click", function(){
+    window.scroll({top:0,left:0,behavior:'smooth'});
+})
